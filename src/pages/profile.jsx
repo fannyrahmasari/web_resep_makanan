9000c38f79903f7b54e48de3ca01d640951605ca
Fanny Rahmasari <96949428+fannyrahmasari@users.noreply.github.com>
2023-10-25 11:46:46 +0700

    feat : menambahkan pages profile

---
+++ src/pages/profile.jsx
@@ -0,0 +1,9 @@
+import Navbar from "../components/Navbar"
+
+const Profile = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default Profile
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import LandingPages from "../pages/landingPages"
 import Login from '../pages/auth/login'
 import SignUp from '../pages/auth/signup'
 import AllRecipe from '../pages/allRecipe'
+import Profile from '../pages/profile'
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path='/login' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/allrecipe' element={<AllRecipe />} />
+        <Route path='/profile' element={<Profile />} />
       </Routes>
     </Router>
   )
